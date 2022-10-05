526d3a44d9b1558834fff88eeb517dee4b492f03
hilalsidhic <hilalsidhic21@gmail.com>
2022-10-05 20:29:19 +0530

    story mode added

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((800, 600))
 background = pygame.image.load('background/img_1.png')
 running = True
+count = 0
 # mixer.music.load("title.mp3")
 # mixer.music.play(-1)
 
@@ -20,6 +21,7 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+enemySpeed = 0.3
 enemyImg = []
 enemyX = []
 enemyY = []
@@ -83,6 +85,43 @@ def new_game():
     over_font = pygame.font.Font('freesansbold.ttf', 64)
     game_loop()
 
+def story_new_game(bg,en,enspeed):
+    global playerx,playerY,playerX_change,playerY_change,enemySpeed,enemyImg,enemyX,enemyY,enemyX_change,enemyY_change,num_of_enemies,bulletImg,bulletX,bulletY,bulletX_change,bulletY_change,bullet_state,score_value,font,textX,textY,over_font
+    # playerImg = pygame.image.load('player.png')
+    playerX = 370
+    playerY = 480
+    playerX_change = 0
+    enemySpeed = enspeed
+
+    enemyImg = []
+    enemyX = []
+    enemyY = []
+    enemyX_change = []
+    enemyY_change = []
+    num_of_enemies = 6
+    background = pygame.image.load(bg)
+    enemylink = en
+    for i in range(num_of_enemies):
+        enemyImg.append(pygame.image.load(enemylink))
+        enemyX.append(random.randint(0, 736))
+        enemyY.append(random.randint(30, 150))
+        enemyX_change.append(enemySpeed)
+        enemyY_change.append(0.01)
+
+    bulletX = 0
+    bulletY = 480
+    bulletX_change = 0
+    bulletY_change = 0.5
+    bullet_state = "ready"
+
+    score_value = 0
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    textX = 10
+    textY = 10
+
+    over_font = pygame.font.Font('freesansbold.ttf', 64)
+    story_mode_loop()
+
 def game_over_text():
     green = (0,255,0)
     bright_green = (0,200,0)
@@ -90,9 +129,18 @@ def game_over_text():
     bright_red = (200,0,0)
     over_text = over_font.render("~GAME OVER~", True, (200, 200, 255))
     screen.blit(over_text, (170, 250))
-    button("Restart",150,450,100,100,green,bright_green,game_intro)
+    button("Menu",150,450,100,100,green,bright_green,game_intro)
     button("Quit",350,450,100,100,red,bright_red,quitgame)
 
+def game_won_text():
+    green = (0,255,0)
+    bright_green = (0,200,0)
+    red = (255,0,0)
+    bright_red = (200,0,0)
+    white = (255,255,255)
+    over_text = over_font.render("~GAME WON~", True, red)
+    screen.blit(over_text, white)
+    button("Menu",150,450,100,100,green,bright_green,game_intro)
 
 def show_score(x, y):
     score = font.render("score: " + str(score_value), True, (200, 200, 255))
@@ -289,7 +337,8 @@ def game_intro():
         TextRect.center = ((display_width/2),(display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
 
-        button("NEW GAME",150,450,100,50,green,bright_green,new_game)
+        button("FREEPLAY",150,450,100,50,green,bright_green,new_game)
+        button("STORY MODE",150,550,100,50,green,bright_green,story_mode_loop)
         button("GO!",280,450,100,50,green,bright_green,game_loop)
         button("change Arena",390,450,130,50,green,bright_green,selectBackground)
         button("change Player",390,550,130,50,green,bright_green,selectPlayer)
@@ -379,6 +428,94 @@ def game_loop():
         pygame.display.update()
         # global keys
         
+def story_mode_loop():
+    global playerX,playerY,count,playerX_change,enemySpeed,playerY_change,bulletX,bulletY,bulletX_change,bulletY_change,bullet_state,score_value,enemyX,enemyY,enemyX_change,enemyY_change
+    running = True
+    count=count + 1
+    while running:
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+        
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -0.3
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 0.3
+                if event.key == pygame.K_SPACE:
+                    if bullet_state == "ready":
+                        bulletSound = mixer.Sound("shoot.wav")
+                        bulletSound.play()
+                        bulletX = playerX
+                        fire_bullet(playerX, playerY)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+
+        playerX += playerX_change
+        if playerX <= 0:
+            playerX = 0
+        if playerX >= 706:
+            playerX = 706
+
+        for i in range(num_of_enemies):
+            if enemyY[i] > 450:
+                for j in range(num_of_enemies):
+                    enemyY[j] = 2000
+                game_over_text()
+                break
+            enemyX[i] += enemyX_change[i]
+            enemyY[i] += 1.25*enemyY_change[i]
+            if enemyX[i] <= 0:
+                enemyY[i] += enemyY_change[i]
+                enemyX_change[i] = enemySpeed
+            if enemyX[i] >= 736:
+                enemyY[i] += enemyY_change[i]
+                enemyX_change[i] = -(enemySpeed)
+            if enemyY[i] >= 600:
+                enemyY[i] = random.randint(30, 150)
+
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+                explosionSound = mixer.Sound("explosion.mp3")
+                explosionSound.play()
+                bulletY = 480
+                bullet_state = "ready"
+                score_value += 1
+                enemyX[i] = random.randint(0, 736)
+                enemyY[i] = random.randint(30, 150)
+
+            enemy(enemyX[i], enemyY[i], i)
+
+        if bulletY <= 0:
+            bulletY = 480
+            bullet_state = "ready"
+
+        if bullet_state == "fire":
+            screen.blit(bulletImg, (bulletX + 32, bulletY - 12))
+            bulletY -= bulletY_change
+        
+        if score_value >= 5 and count ==1:
+            running = False
+            story_new_game('background/img.png','enemy/enemy.png',0.5)
+
+        if score_value >= 5 and count ==2:
+            # running = False
+            game_won_text()
+        
+
+        player(playerX, playerY)
+        show_score(textX, textY)
+        pygame.display.update()
+        # global keys
+
 game_intro()
 # game_loop()
 pygame.quit()
